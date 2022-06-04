c9ec7745dcb49e31211b1d4c3b54e0f4559f541b
Felipe Giraldo <felipe-giraldo@hotmail.es>
2022-06-03 21:53:41 -0500

    structure fixes

--- Frontend-Employee-angular-main/src/app/components/nav/nav.component.ts
+++ Frontend-Employee-angular-main/src/app/components/nav/nav.component.ts
@@ -1,25 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { StoreService } from 'src/app/services/store.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
 
   activeMenu:boolean = false;
   counter = 0;
 
   constructor(
-    private storeService: StoreService
   ) { }
 
-  ngOnInit(): void {
-    this.storeService.myCart$.subscribe(products =>{
-      this.counter = products.length;
-    });
-  }
 
   toggleMenu(){
     this.activeMenu = !this.activeMenu;
--- Frontend-Employee-angular-main/src/app/pipes/vocales.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { VocalesPipe } from './vocales.pipe';
-
-describe('VocalesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new VocalesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
